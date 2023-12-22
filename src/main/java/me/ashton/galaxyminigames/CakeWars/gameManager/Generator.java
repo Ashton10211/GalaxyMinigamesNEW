9df6d85acbdc9a189004340a26da0dba08926a97
Ashton10211 <aamouri1021@icloud.com>
2023-12-22 02:03:27 -0500

    Generator redone / loop to be done tomorrow revise code and start working on real mechanics

--- src/main/java/me/ashton/galaxyminigames/CakeWars/gameManager/Generator.java
+++ src/main/java/me/ashton/galaxyminigames/CakeWars/gameManager/Generator.java
@@ -16,99 +16,33 @@ import java.util.UUID;
 public class Generator {
 
 
-
     private CakeConstructor cakeConstructor;
+    private TeamInfo teamInfo;
     int Level;
-    public Generator(CakeConstructor cakeConstructor, Map<UUID, Integer> playerLevels) {
-        this.playerLevels = playerLevels;
+
+    public Generator(CakeConstructor cakeConstructor, TeamInfo teamInfo) {
         this.cakeConstructor = cakeConstructor;
-
-
-    }
-
-
-    private final Map<UUID, Integer> playerLevels;
-
-
-
-
-    public void setPlayerLevels(UUID playerId, int level) {
-        playerLevels.put(playerId, level);
-
-    }
-
-    public int getPlayerLevel(UUID playerId) {
-        return playerLevels.getOrDefault(playerId, 1);
+        this.teamInfo = teamInfo;
 
     }
 
 
-    @EventHandler
-    public void onJoin(PlayerJoinEvent event) {
 
 
-        setPlayerLevels(event.getPlayer().getUniqueId(), 2); // this is for testing purposes I want it to be set at 2
-    }
-    public void testGenerate() {
+    public void onGenerate() {
+
         for (Player player : Bukkit.getOnlinePlayers()) {
-            if (getPlayerLevel(player.getUniqueId()) == 2) {
-                player.sendMessage("You are level 2");
-            }
-            if (getPlayerLevel(player.getUniqueId()) == 3) {
-                player.sendMessage("You are level 3 (ISLAND)");
+
+            int playerLevel = teamInfo.getTeamLevel(player.getUniqueId());
+            if(playerLevel == 1) {
+                player.getInventory().addItem(new ItemStack(Material.IRON_INGOT));
+                player.sendMessage("You are level 1 by the way so I gave you an iron Ingot "); // Base support for testing
+
             }
 
         }
     }
+
+
 }
-   /* public void globalGenerate() {
-        if (cakeConstructor.isStarted()) {
-            for (World world : Bukkit.getWorlds()) {
-                for (Chunk chunk : world.getLoadedChunks()) {
-                    for (BlockState blockState : chunk.getTileEntities()) {
-                        if (blockState.getType() == Material.DIAMOND_BLOCK) {
-                            Block block = blockState.getBlock();
-                            block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(Material.IRON_INGOT));
-                        }
-                        else if(blockState.getType() == Material.IRON_BLOCK ) {
-                            Block block = blockState.getBlock();
-                            block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(Material.EMERALD));
-                        }
-                        else if(blockState.getType() == Material.GOLD_BLOCK) {
-                            Block block = blockState.getBlock();
-                            block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(Material.DIAMOND));
-                        }
-                    }
-                }
-            }
-        }
-    }
-        public void onGenerate(Player player, Block block) {
-        if (cakeConstructor.isStarted()) {
-            Location location = player.getLocation();
-            Material level1block = Material.DIAMOND_BLOCK;
-            if (block.getType() == level1block) {
-                generateItem(player);
-            }
-        } else {
-            player.sendMessage("game not started");
-
-        }
-    }
-
-
-    ItemStack level1item = new ItemStack(Material.IRON_INGOT);
-    ItemStack level2item = new ItemStack(Material.DIAMOND);
-
-    public void generateItem(Player pLayer) {
-        if(Level == 1) {
-            pLayer.getInventory().addItem(new ItemStack(level1item));
-        }
-        else if(Level == 2) {
-            pLayer.getInventory().addItem(new ItemStack(level2item));
-        }
-
-    }
-      */
-
 
